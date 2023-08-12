adcd319ef30ba125c5527abd2d799208363bf424
giasuddin <giasuddin.me90@gmail.com>
2023-08-12 07:38:31 +0600

    add fargate configuration

---
+++ ecs.tf
@@ -0,0 +1,64 @@
+resource "aws_ecs_cluster" "ecs" {
+  name = "app_cluster"
+}
+
+resource "aws_ecs_service" "service" {
+  name = "app_service"
+  cluster                = aws_ecs_cluster.ecs.arn
+  launch_type            = "FARGATE"
+  enable_execute_command = true
+
+  deployment_maximum_percent         = 200
+  deployment_minimum_healthy_percent = 100
+  desired_count                      = 1
+  task_definition                    = aws_ecs_task_definition.td.arn
+
+  network_configuration {
+    assign_public_ip = true
+    security_groups  = [aws_security_group.sg.id]
+    subnets          = [aws_subnet.sn1.id, aws_subnet.sn2.id, aws_subnet.sn3.id]
+  }
+}
+
+resource "aws_ecs_task_definition" "td"  {
+  container_definitions = jsonencode([
+    {
+      name         = "app"
+      image        = "***.dkr.ecr.us-east-2.amazonaws.com/app_repo"
+      cpu          = 256
+      memory       = 512
+      essential    = true
+      portMappings = [
+          {
+            "name": "django-celery-private-80-tcp",
+            "containerPort": 80,
+            "hostPort": 80,
+            "protocol": "tcp",
+            "appProtocol": "http"
+        },
+        {
+            "name": "django-celery-private-8000-tcp",
+            "containerPort": 8000,
+            "hostPort": 8000,
+            "protocol": "tcp",
+            "appProtocol": "http"
+        },
+        {
+            "name": "django-celery-private-6379-tcp",
+            "containerPort": 6379,
+            "hostPort": 6379,
+            "protocol": "tcp",
+            "appProtocol": "http"
+        }
+    ]
+    }
+  ])
+  family                   = "app"
+  requires_compatibilities = ["FARGATE"]
+
+  cpu                = "256"
+  memory             = "512"
+  network_mode       = "awsvpc"
+  task_role_arn      = "arn:aws:iam::***:role/ecsTaskExecutionRole"
+  execution_role_arn = "arn:aws:iam::***:role/ecsTaskExecutionRole"
+}
